604aad8056573bf2432b2d111bea4d0a6663e208
Musatfa Gamal Abbas <Mustafapiso@outlook.com>
2017-08-15 23:34:57 +0200

    handle some reviewed issues

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndPointResonse.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndPointResonse.java
@@ -4,12 +4,13 @@ package com.udacity.gradle.builditbigger;
  * Created by pisoo on 8/12/2017.
  */
 
+import org.junit.Assert;
 import org.junit.Test;
 
  public class EndPointResonse {
     @Test
     public void testResponse() {
         String joke = String.valueOf(new EndpointsAsyncTask(null).doInBackground());
-        assert (joke.isEmpty());
+        Assert.assertNotNull (joke.isEmpty());
      }
 }
